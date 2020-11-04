b209373fa090de6838fee680fb2c9b3389535363
Harinder Gakhal <harinder.gakhal@gmail.com>
2020-11-03 23:07:12 -0800

    Add blinking cursor effect to title

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var i = 0;
 var title = 'Harinder Gakhal';
-var speed = 70;
+var speed = 50;
 
 // Modidied version of: https://www.w3schools.com/howto/howto_js_typewriter.asp
 function typeWriter() {
 	if (i < title.length) {
-		document.getElementById("Title").innerHTML += title.charAt(i);
+		document.getElementById("Title").innerHTML = title.substring(0, i+1) + "|";
 		i++;
 		setTimeout(typeWriter, (speed + (Math.floor((Math.random() * 70) + 5))));
 	}
+	if (document.getElementById("Title").innerHTML == (title + "|")) {
+		blinkingCursor()
+	}
+}
+
+var blink = true
+function blinkingCursor() {
+	if (blink == true) {
+		document.getElementById("Title").innerHTML = title.substring(0, i+1) + "&ensp;";
+		blink = false
+	}
+	else {
+		document.getElementById("Title").innerHTML = title.substring(0, i+1) + "|";
+		blink = true
+	}
+	setTimeout(blinkingCursor, 530);
 }
 
 typeWriter()
